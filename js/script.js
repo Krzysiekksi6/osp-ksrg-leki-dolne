3ffdbba6d3e1e22a43bdccd22137f6863e3d526d
Krzysztof <krzysiekksi6@gmail.com>
2023-10-02 19:55:31 +0200

    feature/section-history

---
+++ js/script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const nav = document.querySelector(".navbar");
+  const navItems = document.querySelectorAll(".nav-link");
+  const navList = document.querySelector(".navbar-collapse");
+
+  const addShadow = () => {
+    if (window.scrollY >= 300) {
+      nav.classList.add("shadow-bg");
+    } else {
+      nav.classList.remove("shadow-bg");
+    }
+  };
+
+  navItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      navList.classList.remove("show");
+    });
+  });
+  window.addEventListener("scroll", addShadow);
+});
